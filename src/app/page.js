5566f9e396be678c6fa5eeed6cdb72c3e05739a7
Gboly <gbolahanahmed1@gmail.com>
2024-11-22 01:25:29 +0100

    feat: Add bannner image

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 "use client";
 
+import Image from "next/image";
 import "./page.css";
 import Nav from "@/components/nav/Nav";
+import bgImage from "@/images/bg.png";
 
 const Home = () => {
   return (
     <div>
       <Nav />
+      <Image src={bgImage} alt="background image" className="bg-img" />
     </div>
   );
 };
